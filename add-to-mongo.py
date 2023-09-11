2d4828aeb9ccdfc638cd24b8ca9d7b328f2793e6
Georg Stach <info@GeorgStach.de>
2023-09-11 18:03:43 +0200

    MongoUrl as first argument; moved test resources

--- add-to-mongo.py
+++ add-to-mongo.py
@@ -1,22 +1,25 @@
 import os
 import json
 import sys
+import traceback
+
 import pymongo
 
-connection_url = os.environ.get("mongoDbConnectionUrl")
 db_name = "flight-tracker"
 collection_name = "flights-HAM"
+mongo_url = sys.argv[1]
 
 # JSON-Daten von der Standardeingabe lesen
 json_data = input()
-print(json_data)
 
 try:
     # JSON-Array in Python-Liste umwandeln
     flights = json.loads(json_data.replace('\n', '').replace('\r', ''))
 
+    print(flights)
+
     if len(flights) > 0:
-        db_client = pymongo.MongoClient(connection_url)
+        db_client = pymongo.MongoClient(mongo_url)
         db = db_client[db_name]
         collection = db[collection_name]
 
@@ -25,14 +28,19 @@ try:
         db_client.close()
 
         print(f"Anzahl der eingefügten Datensätze: {len(result.inserted_ids)}")
+        sys.exit(0)
     else:
         print("Keine Daten zum Einfügen gefunden.")
+        sys.exit(0)
 except json.JSONDecodeError as e:
     print(f"Fehler beim Decodieren des JSON: {e}")
+    traceback.print_exc()
     sys.exit(1)  # Beendet das Skript mit dem Fehlercode 1
 except pymongo.errors.PyMongoError as e:
+    traceback.print_exc()
     print(f"Fehler beim Einfügen der Datensätze in MongoDB: {e}")
     sys.exit(2)
 except Exception as e:
+    traceback.print_exc()
     print(f"Unerwarteter Fehler: {e}")
     sys.exit(3)
