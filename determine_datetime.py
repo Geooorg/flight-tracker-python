636b82ada18d9367a93a483848045ded43be53fc
Georg Stach <info@GeorgStach.de>
2023-09-16 21:04:10 +0200

    added determine_datetime.py

---
+++ determine_datetime.py
@@ -0,0 +1,18 @@
+import datetime
+from zoneinfo import ZoneInfo
+
+class UnixTimestampSplitter:
+
+    @staticmethod
+    def split_unix_timestamp(timestamp):
+        # Konvertiere den Unix-Timestamp in ein datetime-Objekt
+        result_tuple = datetime.datetime.fromtimestamp(timestamp, tz=ZoneInfo("Europe/Berlin"))
+
+        year = result_tuple.year
+        month = result_tuple.month
+        day = result_tuple.day
+        hour = result_tuple.hour
+        minute = result_tuple.minute
+        second = result_tuple.second
+
+        return year, month, day, hour, minute, second
---
+++ determine_datetime_test.py
@@ -0,0 +1,23 @@
+# Testklasse für die UnixTimestampSplitter-Klasse
+import unittest
+
+from determine_datetime import UnixTimestampSplitter
+
+
+class TestUnixTimestampSplitter(unittest.TestCase):
+    def test_split_unix_timestamp(self):
+        timestamp = 1694871429
+        splitter = UnixTimestampSplitter()
+        year, month, day, hour, minute, seconds = splitter.split_unix_timestamp(timestamp)
+
+        assert year == 2023
+        assert month == 9
+        assert day == 16
+        assert hour == 15
+        assert minute == 37
+        assert seconds == 9
+
+
+if __name__ == "__main__":
+    test = TestUnixTimestampSplitter()
+    test.test_split_unix_timestamp()
